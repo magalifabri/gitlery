a1e5157fc3126fbda8a1971bc34e5f5ec8823224
Magali Fabri <magali_90@live.nl>
2022-04-20 21:27:04 +0200

    Remove unnecessary files

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
-
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
     const [data, setData] = React.useState(null);
@@ -38,12 +10,9 @@ function App() {
     }, []);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>{!data ? "Loading..." : data}</p>
-            </header>
-        </div>
+        <>
+            <p>{!data ? "Loading..." : data}</p>
+        </>
     );
 }
 
