1aba4f62ed04bfe9e13c9498db1bec7710fb3ac6
Magali Fabri <magali_90@live.nl>
2022-04-21 16:31:55 +0200

    Call api with user input and store response data

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
-function App() {
-    const [data, setData] = React.useState(null);
 
-    React.useEffect(() => {
-        fetch("/api")
+const App = () => {
+    // const [data, setData] = useState(null);
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+
+    // useEffect(() => {
+    //     fetch("/api")
+    //         .then((res) => res.json())
+    //         .then((data) => setData(data.message));
+    // }, []);
+
+
+    const getRepos = async requestedUser => {
+        await fetch(`/get-repos?requested-user=${requestedUser}`)
             .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
+            .then((data) => setRepos(data.repos));
+    }
+
+
+    const handleInputKeyDown = async ({key, target}) => {
+        if (key === 'Enter') {
+            const requestedUser = target.value;
+
+            setLoading(true);
+            await getRepos(requestedUser);
+            setLoading(false);
+
+            console.log(repos);
+        }
+    };
+
 
     return (
         <>
-            <p>{!data ? "Loading..." : data}</p>
+            <input onKeyDown={handleInputKeyDown} type="text"/>
+            {/*<p>{!data ? "Loading..." : data}</p>*/}
+            {loading ? "Loading..." : null}
         </>
     );
 }
