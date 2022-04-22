a4289e7976bc8c14c108e04291961db3387edb70
Magali Fabri <magali_90@live.nl>
2022-04-22 14:31:42 +0200

    Add basic styling to repo cards for layout and appearance

--- client/src/components/RepoCards.js
+++ client/src/components/RepoCards.js
@@ -2,16 +2,30 @@ import React from 'react';
 import '../styling/RepoCards.scss';
 
 const RepoCards = ({repos}) => {
+
+    const getClassNames = (event) => {
+        const width = event.target.naturalWidth;
+        const height = event.target.naturalHeight;
+
+        if (width > height) {
+            event.target.parentNode.classList.add('repo-card--wide');
+        } else {
+            event.target.parentNode.classList.add('repo-card--tall');
+        }
+    }
+
+
     return (
-        <div className="repo-cards-wrapper">
+        <div className="grid-wrapper">
             {
                 repos.map(repo => {
                     return (
-                        <div className="card">
-                            <img className="card__image"
-                                 src={repo.screenshotUrl} alt=""/>
-                            <a className="card__name"
-                               href={repo.html_url}>{repo.name}</a>
+                        <div className="repo-card">
+                            <img className="repo-card__screenshot"
+                                src={repo.screenshotUrl} alt=""
+                                onLoad={getClassNames}
+                            />
+                            <a className="repo-card__link" href={repo.html_url}>{repo.name}</a>
                         </div>
                     )
                 })
